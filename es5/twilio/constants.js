de728b1c2e958e4dacd2f785a9791ce84ca82d5b
Ryan Rowland <ryan.w.rowland@gmail.com>
2019-06-07 14:07:56 -0500

    1.7.4-dev

--- es5/twilio/constants.js
+++
@@ -1,4 +0,0 @@
-var pkg = require('../../package.json');
-module.exports.SOUNDS_BASE_URL = 'https://media.twiliocdn.com/sdk/js/client/sounds/releases/1.0.0';
-module.exports.RELEASE_VERSION = pkg.version;
-//# sourceMappingURL=constants.js.map
