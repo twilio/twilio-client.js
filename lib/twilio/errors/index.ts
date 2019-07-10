0954db6f94cab3c612be7823930ae1f23b3e134e
Ryan Rowland <ryan.w.rowland@gmail.com>
2019-07-10 15:38:02 -0500

    CLIENT-6379 | Add error generation script

---
+++ lib/twilio/errors/index.ts
@@ -0,0 +1,25 @@
+import { errorsByCode, MediaErrors, TwilioError } from './generated';
+
+// Application errors that can be avoided by good app logic
+export class InvalidArgumentError extends Error {
+  type: 'InvalidArgumentError';
+}
+export class InvalidStateError extends Error {
+  type: 'InvalidStateError';
+}
+export class NotSupportedError extends Error {
+  type: 'NotSupportedError';
+}
+
+// This should only be used to look up error codes returned by a server
+// using the same repo of error codes.
+export function getErrorByCode(code: number): TwilioError {
+  const error: TwilioError | undefined = errorsByCode.get(code);
+  if (!error) {
+    throw new InvalidArgumentError(`Error code ${code} not found`);
+  }
+  return error;
+}
+
+// All errors we want to throw or emit locally in the SDK need to be passed through here.
+export { MediaErrors, TwilioError };
