a9a55c65a1268abec0878acbab4e4946a3ecf29d
twilio-ci <twilio-ci@twilio.com>
2019-07-23 20:04:21 +0000

    1.7.6

---
+++ es5/twilio/constants.js
@@ -0,0 +1,4 @@
+var pkg = require('../../package.json');
+module.exports.SOUNDS_BASE_URL = 'https://media.twiliocdn.com/sdk/js/client/sounds/releases/1.0.0';
+module.exports.RELEASE_VERSION = pkg.version;
+//# sourceMappingURL=constants.js.map
