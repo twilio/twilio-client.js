d4b7373e6d53b038f7a0617b2f57c18d427b271d
Ryan Rowland <ryan.w.rowland@gmail.com>
2019-07-29 15:41:07 -0500

    CLIENT-6392 | Remove package.json dependency

--- templates/constants.js
+++ templates/constants.js
@@ -1,3 +1,10 @@
 const RELEASE_VERSION = '$version';
 module.exports.SOUNDS_BASE_URL = 'https://media.twiliocdn.com/sdk/js/client/sounds/releases/1.0.0';
 module.exports.RELEASE_VERSION = RELEASE_VERSION;
+
+/**
+ * All errors we plan to use need to be defined here.
+ */
+module.exports.USED_ERRORS = [
+  'MediaErrors.UserMediaDenied',
+];
