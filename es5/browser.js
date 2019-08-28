cd3267c480229c6dd016038ab8314f7548df33f3
twilio-ci <twilio-ci@twilio.com>
2019-08-28 17:12:13 +0000

    1.8.1

---
+++ es5/browser.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*! $name.js $version
+
+$license
+ */
+/* eslint-disable */
+(function (root) {
+  var bundle = $bundle;
+  var Voice = bundle($entry);
+  /* globals define */
+  if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return Voice;
+    });
+  } else {
+    var Twilio = root.Twilio = root.Twilio || {};
+    Twilio.Connection = Twilio.Connection || Voice.Connection;
+    Twilio.Device = Twilio.Device || Voice.Device;
+    Twilio.PStream = Twilio.PStream || Voice.PStream;
+  }
+})(typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : undefined);
