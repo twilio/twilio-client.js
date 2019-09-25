fc491193917013df7f6d7b5d6432ac0880a3c3ec
charliesantos <csantos@twilio.com>
2019-09-25 12:10:36 -0700

    1.7.7-dev

--- es5/twilio/constants.js
+++
@@ -1,7 +0,0 @@
-/**
- * This file is generated on build. To make changes, see /templates/constants.js
- */
-var RELEASE_VERSION = '1.7.7-rc1';
-module.exports.SOUNDS_BASE_URL = 'https://media.twiliocdn.com/sdk/js/client/sounds/releases/1.0.0';
-module.exports.RELEASE_VERSION = RELEASE_VERSION;
-//# sourceMappingURL=constants.js.map
