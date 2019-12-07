133b00f212d2625ded08d2ba57983e078b3c3475
twilio-ci <twilio-ci@twilio.com>
2019-12-07 02:17:52 +0000

    1.9.7

---
+++ es5/twilio/constants.js
@@ -0,0 +1,24 @@
+/**
+ * This file is generated on build. To make changes, see /templates/constants.js
+ */
+var RELEASE_VERSION = '1.9.7';
+module.exports.SOUNDS_BASE_URL = 'https://media.twiliocdn.com/sdk/js/client/sounds/releases/1.0.0';
+module.exports.RELEASE_VERSION = RELEASE_VERSION;
+/**
+ * All errors we plan to use need to be defined here.
+ */
+module.exports.USED_ERRORS = [
+    'AuthorizationErrors.AccessTokenExpired',
+    'AuthorizationErrors.AccessTokenInvalid',
+    'AuthorizationErrors.AuthenticationFailed',
+    'ClientErrors.BadRequest',
+    'GeneralErrors.ConnectionError',
+    'GeneralErrors.TransportError',
+    'GeneralErrors.UnknownError',
+    'MediaErrors.ClientLocalDescFailed',
+    'MediaErrors.ClientRemoteDescFailed',
+    'MediaErrors.ConnectionError',
+    'SignalingErrors.ConnectionDisconnected',
+    'SignalingErrors.ConnectionError',
+];
+//# sourceMappingURL=constants.js.map
