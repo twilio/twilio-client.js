78ba7123fca92562de85c9e8c04aa114bbe1ad9a
charliesantos <csantos@twilio.com>
2020-02-10 13:36:20 -0800

    CLIENT-7221 | Updating sound base url to use sdk.twilio.com

--- templates/constants.js
+++ templates/constants.js
@@ -1,5 +1,5 @@
 const RELEASE_VERSION = '$version';
-module.exports.SOUNDS_BASE_URL = 'https://media.twiliocdn.com/sdk/js/client/sounds/releases/1.0.0';
+module.exports.SOUNDS_BASE_URL = 'https://sdk.twilio.com/js/client/sounds/releases/1.0.0';
 module.exports.RELEASE_VERSION = RELEASE_VERSION;
 
 /**
