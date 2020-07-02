2ecc18de5f89f73815e7c1828acedfb9e7bdc14d
twilio-ci <twilio-ci@twilio.com>
2020-07-02 20:49:11 +0000

    1.13.0-dev

--- es5/twilio.d.ts
+++
@@ -1,5 +0,0 @@
-import Connection from './twilio/connection';
-import Device from './twilio/device';
-import { PreflightTest } from './twilio/preflight/preflight';
-declare const PStream: any;
-export { Connection, Device, PreflightTest, PStream };
--- es5/twilio/preflight/timing.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * @packageDocumentation
- * @module Voice
- * @publicapi
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=timing.js.map
