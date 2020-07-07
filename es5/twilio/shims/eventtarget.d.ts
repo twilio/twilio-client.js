3bba783f1b1d300374e9d10636597cf8a4be27e9
twilio-ci <twilio-ci@twilio.com>
2020-07-07 20:17:14 +0000

    1.13.0-dev

--- es5/twilio/shims/eventtarget.d.ts
+++
@@ -1,8 +0,0 @@
-export = EventTarget;
-declare function EventTarget(): void;
-declare class EventTarget {
-    dispatchEvent(event: any): any;
-    addEventListener(...args: any[]): any;
-    removeEventListener(...args: any[]): any;
-    _defineEventHandler(eventName: any): void;
-}
