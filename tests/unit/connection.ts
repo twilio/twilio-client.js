d90e723a7bf5ee0651159d0bbc91512ffa079cea
Michael Huynh <12516091+mhuynh5757@users.noreply.github.com>
2021-01-13 12:10:35 -0800

    [CLIENT-7730] Allow Connection specific ICE Servers (#245)
    * Allow Connection specific ICE Servers
    
    * Change override to `rtcConfiguration`.
    
    * Update CHANGELOG.md

--- tests/unit/connection.ts
+++ tests/unit/connection.ts
@@ -492,6 +492,16 @@ describe('Connection', function() {
           });
         });
 
+        it('should call mediaStream.answerIncomingCall with override `rtcConfiguration`', () => {
+          const rtcConfiguration = {
+            iceServers: [{ urls: 'foo-ice-server-url' }],
+          };
+          conn.accept(undefined, rtcConfiguration);
+          return wait.then(() => {
+            assert.deepEqual(mediaStream.answerIncomingCall.args[0][3], rtcConfiguration);
+          });
+        });
+
         context('when the success callback is called', () => {
           it('should publish an accepted-by-local event', () => {
             conn.accept();
@@ -562,6 +572,16 @@ describe('Connection', function() {
             sinon.assert.calledOnce(mediaStream.makeOutgoingCall);
             assert.equal(mediaStream.makeOutgoingCall.args[0][1],
               'To=foo&a=undefined&b=true&c=false&d=&e=123&f=123&g=null&h=undefined&i=null&j=0&k=0&l=a%24b%26c%3Fd%3De');
+          });
+        });
+
+        it('should call mediaStream.makeOutgoingCall with an override `rtcConfiguration`', () => {
+          const rtcConfiguration = {
+            iceServers: [{ urls: 'foo-ice-server-url' }],
+          };
+          conn.accept(undefined, rtcConfiguration);
+          return wait.then(() => {
+            assert.deepEqual(mediaStream.makeOutgoingCall.args[0][4], rtcConfiguration);
           });
         });
 
