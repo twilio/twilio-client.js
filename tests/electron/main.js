37f1bda6902a46c280ba8f3a7349fbfa0f9ebb96
Michael Huynh <12516091+mhuynh5757@users.noreply.github.com>
2022-04-27 15:27:55 -0700

    redact leaked credentials (#303)

--- tests/electron/main.js
+++ tests/electron/main.js
@@ -11,8 +11,7 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL('http://twilio:video@localhost:8080/?client=rrowland&twiliojs=http://localhost:1234/twilio.js');
-  //win.loadURL('https://twilio:video@webrtc-phone-2.appspot.com');
+  win.loadURL(/* REDACTED */)
 
   // Open the DevTools.
   win.webContents.openDevTools()
